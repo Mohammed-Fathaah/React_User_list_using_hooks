545a280187541889d6605d52c403d6b752d8dbf5
Mohammed-Fathaah <mohammedfathaah@gmail.com>
2026-02-04 12:02:39 +0530

    userlist.jsx done

---
+++ src/Components/Userlist.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Userlist.css";
+
+function Userlist({ users }) {
+    if (users.length === 0) {
+        return <p>Loading users...</p>;
+    }
+
+    return (
+        <div className="card-container">
+            {users.map((user)=>(
+                <div className="card" key={user.id}>
+                    <h3>{username}</h3>
+                    <p>{user.email}</p>
+                    <span className="role">{user.role}</span>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Userlist;
